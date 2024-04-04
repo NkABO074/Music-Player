c81268974dcc9682015ebcb0f466fbf1de32534d
Nkwe <nkabo074@gmail.com>
2024-04-04 15:50:49 +0100

    you can now have acces to the playlist and change music

--- src/script.js
+++ src/script.js
@@ -2,6 +2,33 @@ const MUSIC = document.querySelector("audio");
 const PREV_BTN = document.getElementById("prev");
 const PLAY_BTN = document.getElementById("play");
 const NEXT_BTN = document.getElementById("next");
+const TITLE = document.getElementById("title");
+const ARTIST = document.getElementById("artist");
+const IMAGE = document.querySelector("img");
+
+// Our Tracs
+const SONGS = [
+  {
+    name: "music-1",
+    display_name: "Electric chill Machine",
+    artist: "John Doe",
+  },
+  {
+    name: "music-2",
+    display_name: "Chill Dude",
+    artist: "John Doe",
+  },
+  {
+    name: "music-3",
+    display_name: "Dawg",
+    artist: "John Doe",
+  },
+  {
+    name: "music-4",
+    display_name: "I am HIM",
+    artist: "John Doe",
+  },
+];
 
 // Check if playing
 let is_playing = false;
@@ -30,3 +57,48 @@ function pauseSong() {
 PLAY_BTN.addEventListener("click", () =>
   is_playing ? pauseSong() : playSong()
 );
+
+/**
+ * Update the DOM
+ * @param {{name: string;display_name: string;artist: string;}[]} song object holding the sound infos
+ * */
+function loadSong(song) {
+  TITLE.textContent = song.display_name;
+  ARTIST.textContent = song.artist;
+  MUSIC.src = `./assets/music/${song.name}.mp3`;
+  IMAGE.src = `./assets/img/${song.name}.jpg`;
+}
+
+// Current song
+let song_index = 0;
+
+/**
+ * Previous song
+ */
+function prevSong() {
+  song_index--;
+  if (song_index < 0) {
+    song_index = SONGS.length - 1;
+  }
+  loadSong(SONGS[song_index]);
+  playSong();
+}
+
+/**
+ * Next song
+ */
+function nextSong() {
+  song_index++;
+  if (song_index >= SONGS.length - 1) {
+    song_index = 0;
+  }
+  loadSong(SONGS[song_index]);
+  playSong();
+}
+
+// On load select first song
+loadSong(SONGS[song_index]);
+
+// Envent listeners
+PREV_BTN.addEventListener("click", prevSong);
+NEXT_BTN.addEventListener("click", nextSong);
